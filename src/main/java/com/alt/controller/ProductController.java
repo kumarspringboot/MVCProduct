22d2850d5de2dc14e900c04b556eeaa840617a18
kumar <kumarmhka@gmail.com>
2025-08-14 22:12:01 +0530

    .s

--- src/main/java/com/alt/controller/ProductController.java
+++ src/main/java/com/alt/controller/ProductController.java
@@ -3,23 +3,21 @@ package com.alt.controller;
 import com.alt.dto.ProductDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
 
-    @GetMapping(name = "/")
+    @RequestMapping(value = "/")
     public String getRegistrationPage(Model model){
     model.addAttribute("product",new ProductDTO());
     return "register";
     }
 //
-//    @PostMapping("/register")
-//    public String registerProduct(@ModelAttribute("product") ProductDTO product, Model model) {
-//        // code to save product via service
-//        return "success"; // or redirect page
-//    }
+    @PostMapping(value="/register")
+    public String registerProduct(@ModelAttribute("product") ProductDTO product, Model model) {
+        // code to save product via service
+        return "success"; // or redirect page
+    }
 
 }
